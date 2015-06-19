438b5c474c89cd1df313f8546d8f6944feee8f96
Zain Mustafa <zmdhillo@gmail.com>
2015-06-19 12:30:19 -0700

    latest

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-console.log("Hellow world");
+var Human = (function () {
+    function Human(name) {
+        this.name = name;
+    }
+    Human.prototype.eat = function () {
+        console.log(this.name + " is a Human and is eating");
+    };
+    return Human;
+})();
+var Robot = (function () {
+    function Robot(name) {
+        this.name = name;
+    }
+    return Robot;
+})();
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.eat = function () {
+        console.log(this.name + " is a Human and is eating");
+    };
+    return Animal;
+})();
+var r = new Robot("R2-D2");
+var r0 = new Animal("Donkey"); //how is this possible?
+var isItRobot = r0 instanceof Robot;
+console.log("Is Donkey a Robot: " + isItRobot); //false, giving the right results
+var h = new Human("Tom");
+console.log(h.eat());
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,34 @@
-console.log("Hellow world");
+class Human {
+    name: string;
+    
+    constructor(name: string){
+        this.name = name;
+    }
+    
+    eat(){
+        console.log(this.name + " is a Human and is eating");
+    }
+}
+
+class Robot {
+    name:string;
+    constructor(name:string){
+        this.name = name;
+    }
+}
+class Animal {
+    name:string;
+    constructor(name:string){
+        this.name = name;
+    }
+    eat(){
+        console.log(this.name + " is a Human and is eating");
+    }
+}
+let r: Robot = new Robot("R2-D2");
+let r0: Robot = new Animal("Donkey");//how is this possible?
+
+var isItRobot = r0 instanceof Robot;
+console.log("Is Donkey a Robot: " + isItRobot);//false, giving the right results
+let h: Human = new Human("Tom");
+console.log(h.eat());
